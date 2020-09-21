677ac53e662b6641d61c448182649f6442592db8
sam <machariamaina048@gmail.com>
2020-09-21 14:17:02 +0300

    preview

--- resources/views/pages/previewPortfolio.blade.php
+++ resources/views/pages/previewPortfolio.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.clientside')
 
 @section('content')
+
+<h2>This is the preview of your portfolio</h2>
+
+<div class="cta">
+    <div class="edit">
+
+    </div>
+    <div class="save">
+
+    </div>
+</div>
+<div class="prevw">
+    
+</div>
     
 @endsection
