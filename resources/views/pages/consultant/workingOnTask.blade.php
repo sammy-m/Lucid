a5fc72e6e5e5b4da7577f04a4e3d4583cc4e4c94
sam <machariamaina048@gmail.com>
2020-10-28 15:16:16 +0300

    progress

--- resources/views/pages/consultant/workingOnTask.blade.php
+++ resources/views/pages/consultant/workingOnTask.blade.php
@@ -148,7 +148,25 @@
                 axios.get('/consultant/getfiles?order='+`{{$task[0]['details']->refId}}` ).then( function(res){
                     console.log(res.data[1]);
                     if (res.data[0].length != 0) {
-                        document.getElementById('clntFls').innerHTML = "";
+                        var clientfilesHTML = '';
+                    res.data[0].forEach( file => {
+                        var splt =  file.split('/');
+                       // console.log(splt.length);
+                        var flnm = splt[splt.length - 1];
+                       // console.log(flnm);
+                        var extsplt = flnm.split('.');
+                        var ext = extsplt[extsplt.length - 1];
+                        //console.log(ext);
+                        var Fclass = "";
+                        // if(ext == "word" || ext == 'doc' || ext == 'docx'){
+                        //     Fclass = "word";
+                        // }else if(ext == "pdf"){
+                        //     Fclass = "pdf";
+                        // } else if(ext ==)
+                        clientfilesHTML += `<a href="/file/download?path=`+file+`" class="`+ext+`">`+flnm+`</a> </br>`;
+                    });
+                    document.getElementById('clntFls').innerHTML = clientfilesHTML;
+                        
                     }
                     if (res.data[1].length != 0) {
                         var consultantfilesHTML = '';
